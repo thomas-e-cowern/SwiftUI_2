a2f5f7b630bf99d65882d63a19c072c2c7532184
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-01-09 14:16:13 -0500

    HypedList initial commit

---
+++ HypedList/Shared/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  Shared
+//
+//  Created by Thomas Cowern New on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
