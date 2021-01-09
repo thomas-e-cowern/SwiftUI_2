0cc8e2f145c98a964bde774d960dcd629e8739a2
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-01-09 09:26:26 -0500

    initial commit

---
+++ SwiftUI Basics/SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Thomas Cowern New on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
