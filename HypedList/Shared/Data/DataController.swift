ecdc1815fdbe494455732f3e1e9afd9b6f56eca0
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-01-12 10:33:01 -0500

    added local storage to project

--- HypedList/Shared/Data/DataController.swift
+++ HypedList/Shared/Data/DataController.swift
@@ -12,4 +12,27 @@ class DataController: ObservableObject {
     static var shared = DataController()
     
     @Published  var hypedEvents: [HypedEvent] = []
+    
+    func saveData () {
+        DispatchQueue.global().async {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(self.hypedEvents) {
+                UserDefaults.standard.setValue(encoded, forKey: "hypedEvents")
+                UserDefaults.standard.synchronize()
+            }
+        }
+    }
+    
+    func loadData () {
+        DispatchQueue.global().async {
+            if let data = UserDefaults.standard.data(forKey: "hypedEvents") {
+                let decoder = JSONDecoder()
+                if let jsonHypedEvents = try? decoder.decode([HypedEvent].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.hypedEvents = jsonHypedEvents
+                    }
+                }
+            }
+        }
+    }
 }
