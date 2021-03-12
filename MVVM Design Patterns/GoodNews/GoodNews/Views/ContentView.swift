f094a5418de8784e291b72f10d0ee4ca54d48a90
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-03-12 16:59:17 -0500

    updated status bar color

--- MVVM Design Patterns/GoodNews/GoodNews/Views/ContentView.swift
+++ MVVM Design Patterns/GoodNews/GoodNews/Views/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             
             }
             .navigationBarTitle("Good News")
-            .customNavbarColor(.blue)
+            .customNavbarColor(.gray)
         }
     }
 }
