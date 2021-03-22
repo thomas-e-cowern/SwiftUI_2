5606385d088b6675ec11672e3a9afe5d24ab7959
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-03-22 13:57:55 -0400

    added header

---
+++ Swift14Udemy/Touchdown/Touchdown/Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,42 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Thomas Cowern New on 3/22/21.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    
+    // MARK:  Properties
+    
+    // MARK:  Body
+    var body: some View {
+        HStack {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } // HStack
+    }
+}
+
+// MARK:  Preview
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
---
+++ Swift14Udemy/Touchdown/Touchdown/Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,61 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Thomas Cowern New on 3/22/21.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    
+    // MARK:  Properties
+    @State private var isAnimated : Bool = false
+    
+    // MARK:  Body
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }) // Button
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeIn(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            }) // Button
+            
+        } // End of HStack
+    }
+}
+
+// MARK:  Preview
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
