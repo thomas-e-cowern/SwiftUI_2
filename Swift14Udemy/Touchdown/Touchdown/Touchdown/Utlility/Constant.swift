943dbe902ed9140973bfb182b649ed6d8718e668
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-03-22 09:56:23 -0400

    added folder structure

---
+++ Swift14Udemy/Touchdown/Touchdown/Touchdown/Utlility/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Thomas Cowern New on 3/22/21.
+//
+
+import Foundation
+import SwiftUI
+
+// Color
+
+let colorBackground : Color = Color("ColorBackground")
+let colorGrey : Color = Color(UIColor.systemGray4)
