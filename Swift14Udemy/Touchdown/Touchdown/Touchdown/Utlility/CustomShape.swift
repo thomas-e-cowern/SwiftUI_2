90fdb7dae5eb2983bed110aa2484c067c971b6ed
Thomas E Cowern <thomas.e.cowern@gmail.com>
2021-04-01 14:11:56 -0400

    added product description

---
+++ Swift14Udemy/Touchdown/Touchdown/Touchdown/Utlility/CustomShape.swift
@@ -0,0 +1,22 @@
+//
+//  CustomShape.swift
+//  Touchdown
+//
+//  Created by Thomas Cowern New on 4/1/21.
+//
+
+import SwiftUI
+
+struct CustomShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomShape_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomShape()
+            .previewLayout(.fixed(width: 428, height: 120))
+            .padding()
+    }
+}
--- Swift14Udemy/Touchdown/Touchdown/Touchdown/View/Detail/ProductDetailView.swift
+++ Swift14Udemy/Touchdown/Touchdown/Touchdown/View/Detail/ProductDetailView.swift
@@ -23,18 +23,28 @@ struct ProductDetailView: View {
                 .padding(.horizontal)
             
             // Top detail
+            TopPartDetailView()
+                .padding(.horizontal)
             
             // Bottom detail
-            
-            // Ratings and sizes
-            
-            // Description
-            
-            // Quantity and favorite
-            
-            // Add to cart
-            
-            Spacer()
+            VStack (alignment: .center, spacing: 0, content: {
+                // Ratings and sizes
+                
+                // Description
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                })  // Scroll view
+                // Quantity and favorite
+                
+                // Add to cart
+                
+                Spacer()
+            })  // VStack
+            .padding(.horizontal)
+            .background(Color.white)
         })  // Vstack
         .ignoresSafeArea(.all, edges: .all)
         .background(Color(
