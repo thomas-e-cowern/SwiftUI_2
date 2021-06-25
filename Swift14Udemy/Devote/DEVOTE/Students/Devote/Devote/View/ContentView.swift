5c053fc756332c431c16936d6f9724cdc0d49e2f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2021-06-25 09:48:57 -0400

    updated table

--- Swift14Udemy/Devote/DEVOTE/Students/Devote/Devote/View/ContentView.swift
+++ Swift14Udemy/Devote/DEVOTE/Students/Devote/Devote/View/ContentView.swift
@@ -103,8 +103,15 @@ struct ContentView: View {
                         }
                         .onDelete(perform: deleteItems)
                     } // List
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
                 } // MARK:  VStack
             } // MARK:  ZStack
+            .onAppear() {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            }
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {
                 #if os(iOS)
