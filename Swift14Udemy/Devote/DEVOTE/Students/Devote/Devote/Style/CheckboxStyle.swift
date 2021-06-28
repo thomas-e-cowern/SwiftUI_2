757413a97cf31a0befc4515fe078e118d1f5f693
thomas-e-cowern <thomas.e.cowern@gmail.com>
2021-06-28 10:41:41 -0400

    added checkboxstyle view

---
+++ Swift14Udemy/Devote/DEVOTE/Students/Devote/Devote/Style/CheckboxStyle.swift
@@ -0,0 +1,22 @@
+//
+//  CheckboxStyle.swift
+//  Devote
+//
+//  Created by Thomas Cowern New on 6/28/21.
+//
+
+import SwiftUI
+
+struct CheckboxStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder Label", isOn: .constant(true))
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
