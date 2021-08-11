3e735c4b345cf2275834336ab88485d806b5f854
thomas-e-cowern <thomas.e.cowern@gmail.com>
2021-08-11 18:58:16 -0400

    completed basic widget

--- Swift14Udemy/Devote/DEVOTE/Students/Devote/DevoteWidget/DevoteWidget.swift
+++ Swift14Udemy/Devote/DEVOTE/Students/Devote/DevoteWidget/DevoteWidget.swift
@@ -44,18 +44,42 @@ struct DevoteWidgetEntryView : View {
     var body: some View {
 //        Text(entry.date, style: .time)
         
-        ZStack {
-            backgroundGradient
-            
-            Image("rocket-small")
-                .resizable()
-                .scaledToFit()
-            
-            Image("logo")
-                .resizable()
-                .frame(width: 36, height: 36)
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                
+                Image("rocket-small")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(width: 36, height: 36)
+                    .offset(
+                        x: (geometry.size.width / 2) - 20,
+                        y: (geometry.size.height / -2) + 20)
+                    .padding(.top, 12)
+                    .padding(.trailing, 12)
+                
+                HStack {
+                    Text("Just Do it")
+                        .foregroundColor(.white)
+                        .font(.system(.footnote, design: .rounded))
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(Color(red: 0, green: 0, blue: 0, opacity: 0.5)
+                                        .blendMode(.overlay)
+                        )
+                        .clipShape(Capsule())
+                }
+                // End of HStack
+                .padding()
+                .offset(y: (geometry.size.height / 2) - 24)
+            }
+            // End of ZStack
         }
-        // End of ZStack
+        // End of GeomertyReader
     }
 }
 
